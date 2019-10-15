82df91d079b73e2e7cc01fc65eb3aba200c4dbf7
Anderson Ferreira <decomaxkatsu@gmail.com>
2019-10-14 22:51:08 -0300

    Retornando lista de Categorias

--- src/main/java/com/anderson/cursosb/cursosb/resource/CategoriaResource.java
+++ src/main/java/com/anderson/cursosb/cursosb/resource/CategoriaResource.java
@@ -1,8 +1,14 @@
 package com.anderson.cursosb.cursosb.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.anderson.cursosb.cursosb.domain.Categoria;
 
 /**
  * CategoriaResource
@@ -12,7 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
     
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Está furunfando";    
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
