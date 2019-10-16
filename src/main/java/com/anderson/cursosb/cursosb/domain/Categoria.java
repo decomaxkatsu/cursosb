840a16a5e565d2fbf0719bc7f51262062d6cfce2
Anderson Ferreira <decomaxkatsu@gmail.com>
2019-10-15 21:33:12 -0300

    Inclusão de base h2

--- src/main/java/com/anderson/cursosb/cursosb/domain/Categoria.java
+++ src/main/java/com/anderson/cursosb/cursosb/domain/Categoria.java
@@ -2,13 +2,21 @@ package com.anderson.cursosb.cursosb.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Categoria
  */
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
