b3d02a5b218d69753a2c06c7d493ac29eb985558
Anderson Ferreira <decomaxkatsu@gmail.com>
2019-10-17 21:59:53 -0300

    Service de cliente

---
+++ src/main/java/com/anderson/cursosb/cursosb/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.anderson.cursosb.cursosb.services;
+
+import java.util.Optional;
+
+import com.anderson.cursosb.cursosb.domain.Cliente;
+import com.anderson.cursosb.cursosb.repository.ClienteRepository;
+import com.anderson.cursosb.cursosb.services.Exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * ClienteService
+ */
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente buscar(Integer id){
+        Optional<Cliente> cOptional = clienteRepository.findById(id);
+        return cOptional.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}
